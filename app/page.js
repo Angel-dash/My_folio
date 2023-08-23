acc550d99a0235d5e12fb502bc8af077481c662a
Angel-dash <njlghmr@gmail.com>
2023-08-23 18:21:46 +0545

    mobile first Ui completed

--- app/page.js
+++ app/page.js
@@ -1,113 +1,68 @@
-import Image from 'next/image'
-
+import React from "react"; // Don't forget to import React
+import Image from "next/image";
+import { BsFillMoonStarsFill } from "react-icons/bs"; // Correct the icon import
+import {
+	AiFillTwitterCircle,
+	AiFillLinkedin,
+	AiFillInstagram,
+} from "react-icons/ai"; // Correct the icon import
+import Head from "next/head";
+import kakashi from "../public/kakashi.jpg";
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<div>
+			<Head>
+				<title>Angel's Portfolio</title>
+			</Head>
+			<main className='bg-black px-10'>
+				<section className='min-h-screen'>
+					<nav className='py-10 mb-12 flex justify-between'>
+						<h1 className='text-sm'>developed by Angel</h1>
+						<ul className='flex items-center'>
+							<li>
+								<BsFillMoonStarsFill className='cursor-pointer text-2xl' />
+							</li>
+							<li>
+								<a
+									className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 '
+									href='#'>
+									Resume
+								</a>
+							</li>
+						</ul>
+					</nav>
+					<div className='text-center p-10 py-10'>
+						<h2 className='text-3xl py-2 text-teal-600 font-medium'>
+							Angel Ghimire
+						</h2>
+						<h3 className='text-2xl py-2'>Web developer and Data Science</h3>
+						<p className='text-sm py-5 leading-8 text0=-gray-800'>
+							Hello! I'm passionate about crafting captivating digital
+							experiences through frontend development. With a strong foundation
+							in technologies like React, JavaScript, HTML, and CSS, I'm
+							dedicated to turning ideas into interactive and visually appealing
+							web applications. I specialize in leveraging the power of modern
+							frameworks such as Next.js to create seamless,
+							performance-optimized websites. My proficiency extends to crafting
+							pixel-perfect UIs using tools like Tailwind CSS, ensuring both
+							aesthetic appeal and responsive design. I believe that every line
+							of code contributes to the user's journey, and I'm committed to
+							delivering intuitive and user-friendly interfaces. My journey in
+							frontend development is driven by a curiosity to stay updated with
+							the latest trends and techniques, and a dedication to delivering
+							high-quality, efficient, and elegant solutions.
+						</p>
+					</div>
+					<div className='text-5xl flex justify-center gap-5 py-3 text-gray-600'>
+						<AiFillTwitterCircle />
+						<AiFillInstagram />
+						<AiFillLinkedin />
+					</div>
+					<div className='className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96'>
+						<Image src={kakashi} layout='fill' />
+					</div>
+				</section>
+			</main>
+		</div>
+	);
 }
